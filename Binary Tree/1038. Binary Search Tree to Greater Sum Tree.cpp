9eec1ee36fbdf749e8ab12d675a6ff6370dbcaec
Aman Kumar Tiwari <96365707+aman086@users.noreply.github.com>
2023-03-06 12:47:28 +0530

    Update 1038. Binary Search Tree to Greater Sum Tree.cpp

--- Binary Tree/1038. Binary Search Tree to Greater Sum Tree.cpp
+++ Binary Tree/1038. Binary Search Tree to Greater Sum Tree.cpp
@@ -11,27 +11,26 @@
  */
 
 /*
+  100% Optimal....
+  
   We apply a traversal ...
           right -> root -> left;
           and sum of all the elements , we found in the path between;
           and return that sum variable;
 */
-
-
 class Solution {
 public:
-
-    void postOrder(TreeNode* &root , int& sum){
+    int sum = 0;
+    void postOrder(TreeNode* &root){
         if(root==NULL) return;
-        postOrder(root->right , sum);
+        postOrder(root->right);
         root->val += sum;
         sum = root->val;
-        postOrder(root->left , sum);
+        postOrder(root->left);
     }
     TreeNode* bstToGst(TreeNode* root) {
-        // TreeNode* root = new TreeNode(1);
-        int sum = 0;
-        postOrder(root , sum);
+        postOrder(root);
         return root;
     }
 };
+
