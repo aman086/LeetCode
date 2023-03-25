af3a08b65d213b21ebd4c0e10c289adab333002b
Aman Kumar Tiwari <96365707+aman086@users.noreply.github.com>
2023-03-25 11:03:05 +0530

    Update 105. Construct Binary Tree from Preorder and Inorder Traversal.cpp

--- Binary Tree/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Binary Tree/105. Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -44,3 +44,49 @@ public:
         return root;
     }
 };
+
+
+
+
+
+// second Approach
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int currIndex;
+    unordered_map<int,int> mp;
+    TreeNode* partition(int start , int end , vector<int>& preorder, vector<int>& inorder){
+        if(start > end) return NULL;
+        if(currIndex == preorder.size()) return NULL;
+  
+        int i = mp[preorder[currIndex]];
+        TreeNode* node = new TreeNode(preorder[currIndex++]);
+        node->left = partition(start , i-1 , preorder, inorder);
+        node->right = partition(i+1 , end , preorder , inorder);
+        return node;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        TreeNode* root = NULL;
+        int n = inorder.size();
+
+        // unordered_map<int,int> mp;
+        for(int i=0;i<n;i++){
+            mp[inorder[i]] = i;
+        }
+        currIndex = 0;
+        return partition(0 , n-1 , preorder , inorder);
+    }
+};
+
